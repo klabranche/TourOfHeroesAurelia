9db33f5a35bc09d233d7e5f0cf72d8214105e330
Kevin LaBranche <kevin.labranche@nau.edu>
2017-01-31 11:04:30 -0700

    finished part two of the ng2 tour of heroes using aurelia. https://angular.io/docs/ts/latest/tutorial/toh-pt2.html   Interesting issue that I'm not sure why it's happening.  The selected li element is not highlighting the selected hero if the App class's selectedHero is not originally set to null or an actual hero in the heroes list.  This is vanilla JS so not sure what is wrong.  The translation between ng2 & aurelia should be the same line of code.  Looking at https://github.com/AshleyGrant/tour-of-heroes example, it is the same.

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -3,8 +3,12 @@ define('app',["require", "exports"], function (require, exports) {
     var App = (function () {
         function App() {
             this.title = 'Tour of Heroes';
-            this.hero = { id: 1, name: 'Windstorm' };
+            this.heroes = HEROES;
+            this.selectedHero = null;
         }
+        App.prototype.onSelect = function (hero) {
+            this.selectedHero = hero;
+        };
         return App;
     }());
     exports.App = App;
@@ -14,6 +18,18 @@ define('app',["require", "exports"], function (require, exports) {
         return Hero;
     }());
     exports.Hero = Hero;
+    var HEROES = [
+        { id: 11, name: 'Mr. Nice' },
+        { id: 12, name: 'Narco' },
+        { id: 13, name: 'Bombasto' },
+        { id: 14, name: 'Celeritas' },
+        { id: 15, name: 'Magneta' },
+        { id: 16, name: 'RubberMan' },
+        { id: 17, name: 'Dynama' },
+        { id: 18, name: 'Dr IQ' },
+        { id: 19, name: 'Magma' },
+        { id: 20, name: 'Tornado' }
+    ];
 });
 
 define('environment',["require", "exports"], function (require, exports) {
@@ -54,15 +70,6 @@ define('resources/index',["require", "exports"], function (require, exports) {
     exports.configure = configure;
 });
 
-define('hero',["require", "exports"], function (require, exports) {
-    "use strict";
-    var Hero = (function () {
-        function Hero() {
-        }
-        return Hero;
-    }());
-    exports.Hero = Hero;
-});
-
-define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>${title}</h1><h2>${hero.name} details!</h2><div><label>id:</label>${hero.id}</div><div><label>name:</label><input value.bind=\"hero.name\" placeholder=\"name\"></div></template>"; });
+define('text!app.css', ['module'], function(module) { module.exports = "  .selected {\r\n    background-color: #CFD8DC !important;\r\n    color: white;\r\n  }\r\n  .heroes {\r\n    margin: 0 0 2em 0;\r\n    list-style-type: none;\r\n    padding: 0;\r\n    width: 15em;\r\n  }\r\n  .heroes li {\r\n    cursor: pointer;\r\n    position: relative;\r\n    left: 0;\r\n    background-color: #EEE;\r\n    margin: .5em;\r\n    padding: .3em 0;\r\n    height: 1.6em;\r\n    border-radius: 4px;\r\n  }\r\n  .heroes li.selected:hover {\r\n    background-color: #BBD8DC !important;\r\n    color: white;\r\n  }\r\n  .heroes li:hover {\r\n    color: #607D8B;\r\n    background-color: #DDD;\r\n    left: .1em;\r\n  }\r\n  .heroes .text {\r\n    position: relative;\r\n    top: -3px;\r\n  }\r\n  .heroes .badge {\r\n    display: inline-block;\r\n    font-size: small;\r\n    color: white;\r\n    padding: 0.8em 0.7em 0 0.7em;\r\n    background-color: #607D8B;\r\n    line-height: 1em;\r\n    position: relative;\r\n    left: -1px;\r\n    top: -4px;\r\n    height: 1.8em;\r\n    margin-right: .8em;\r\n    border-radius: 4px 0 0 4px;\r\n  }"; });
+define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"./app.css\"></require><h1>${title}</h1><h2>My Heroes</h2><ul class=\"heroes\"><li repeat.for=\"hero of heroes\" class=\"${hero === selectedHero ? 'selected':''}\" click.delegate=\"onSelect(hero)\"><span class=\"badge\">${hero.id}</span> ${hero.name}</li></ul><div if.bind=\"selectedHero\"><h2>${selectedHero.name} details!</h2><div><label>id:</label>${selectedHero.id}</div><div><label>name:</label><input value.bind=\"selectedHero.name\" placeholder=\"name\"></div></div></template>"; });
 //# sourceMappingURL=app-bundle.js.map
