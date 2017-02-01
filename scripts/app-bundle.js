32bed483f1694ad54a2ef897a4f7d41a0cd75ed0
Kevin LaBranche <kevin.labranche@nau.edu>
2017-02-01 10:29:08 -0700

    This is step 5 (services) of the ng2 tour of heroes tutorial (https://angular.io/docs/ts/latest/tutorial/toh-pt4.html) conversion to Aurelia.  In this step we created a mock web service and learned how to inject it into the application.

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -1,18 +1,16 @@
-define('app',["require", "exports"], function (require, exports) {
+define('hero',["require", "exports"], function (require, exports) {
     "use strict";
-    var App = (function () {
-        function App() {
-            this.title = 'Tour of Heroes';
-            this.heroes = HEROES;
-            this.selectedHero = null;
+    var Hero = (function () {
+        function Hero() {
         }
-        App.prototype.onSelect = function (hero) {
-            this.selectedHero = hero;
-        };
-        return App;
+        return Hero;
     }());
-    exports.App = App;
-    var HEROES = [
+    exports.Hero = Hero;
+});
+
+define('mock-heroes',["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.HEROES = [
         { id: 11, name: 'Mr. Nice' },
         { id: 12, name: 'Narco' },
         { id: 13, name: 'Bombasto' },
@@ -26,6 +24,62 @@ define('app',["require", "exports"], function (require, exports) {
     ];
 });
 
+define('hero-service',["require", "exports", "./mock-heroes"], function (require, exports, mock_heroes_1) {
+    "use strict";
+    var HeroService = (function () {
+        function HeroService() {
+        }
+        HeroService.prototype.getHeroes = function () {
+            return Promise.resolve(mock_heroes_1.HEROES);
+        };
+        HeroService.prototype.getHeroesSlowly = function () {
+            var _this = this;
+            return new Promise(function (resolve) {
+                setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
+            });
+        };
+        return HeroService;
+    }());
+    exports.HeroService = HeroService;
+});
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define('app',["require", "exports", "aurelia-framework", "./hero-service"], function (require, exports, aurelia_framework_1, hero_service_1) {
+    "use strict";
+    var App = (function () {
+        function App(heroService) {
+            this.heroService = heroService;
+            this.title = 'Tour of Heroes';
+            this.selectedHero = null;
+        }
+        App.prototype.activate = function () {
+            this.getHeroes();
+        };
+        App.prototype.getHeroes = function () {
+            var _this = this;
+            this.heroService.getHeroesSlowly()
+                .then(function (heroes) { return _this.heroes = heroes; });
+        };
+        App.prototype.onSelect = function (hero) {
+            this.selectedHero = hero;
+        };
+        return App;
+    }());
+    App = __decorate([
+        aurelia_framework_1.autoinject(),
+        __metadata("design:paramtypes", [hero_service_1.HeroService])
+    ], App);
+    exports.App = App;
+});
+
 define('environment',["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -33,35 +87,6 @@ define('environment',["require", "exports"], function (require, exports) {
         debug: true,
         testing: true
     };
-});
-
-define('main',["require", "exports", "./environment"], function (require, exports, environment_1) {
-    "use strict";
-    Promise.config({
-        warnings: {
-            wForgottenReturn: false
-        }
-    });
-    function configure(aurelia) {
-        aurelia.use
-            .standardConfiguration()
-            .feature('resources');
-        if (environment_1.default.debug) {
-            aurelia.use.developmentLogging();
-        }
-        if (environment_1.default.testing) {
-            aurelia.use.plugin('aurelia-testing');
-        }
-        aurelia.start().then(function () { return aurelia.setRoot(); });
-    }
-    exports.configure = configure;
-});
-
-define('resources/index',["require", "exports"], function (require, exports) {
-    "use strict";
-    function configure(config) {
-    }
-    exports.configure = configure;
 });
 
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -91,14 +116,33 @@ define('hero-detail',["require", "exports", "aurelia-framework", "./hero"], func
     exports.HeroDetail = HeroDetail;
 });
 
-define('hero',["require", "exports"], function (require, exports) {
+define('main',["require", "exports", "./environment"], function (require, exports, environment_1) {
     "use strict";
-    var Hero = (function () {
-        function Hero() {
+    Promise.config({
+        warnings: {
+            wForgottenReturn: false
         }
-        return Hero;
-    }());
-    exports.Hero = Hero;
+    });
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .feature('resources');
+        if (environment_1.default.debug) {
+            aurelia.use.developmentLogging();
+        }
+        if (environment_1.default.testing) {
+            aurelia.use.plugin('aurelia-testing');
+        }
+        aurelia.start().then(function () { return aurelia.setRoot(); });
+    }
+    exports.configure = configure;
+});
+
+define('resources/index',["require", "exports"], function (require, exports) {
+    "use strict";
+    function configure(config) {
+    }
+    exports.configure = configure;
 });
 
 define('text!app.css', ['module'], function(module) { module.exports = "  .selected {\r\n    background-color: #CFD8DC !important;\r\n    color: white;\r\n  }\r\n  .heroes {\r\n    margin: 0 0 2em 0;\r\n    list-style-type: none;\r\n    padding: 0;\r\n    width: 15em;\r\n  }\r\n  .heroes li {\r\n    cursor: pointer;\r\n    position: relative;\r\n    left: 0;\r\n    background-color: #EEE;\r\n    margin: .5em;\r\n    padding: .3em 0;\r\n    height: 1.6em;\r\n    border-radius: 4px;\r\n  }\r\n  .heroes li.selected:hover {\r\n    background-color: #BBD8DC !important;\r\n    color: white;\r\n  }\r\n  .heroes li:hover {\r\n    color: #607D8B;\r\n    background-color: #DDD;\r\n    left: .1em;\r\n  }\r\n  .heroes .text {\r\n    position: relative;\r\n    top: -3px;\r\n  }\r\n  .heroes .badge {\r\n    display: inline-block;\r\n    font-size: small;\r\n    color: white;\r\n    padding: 0.8em 0.7em 0 0.7em;\r\n    background-color: #607D8B;\r\n    line-height: 1em;\r\n    position: relative;\r\n    left: -1px;\r\n    top: -4px;\r\n    height: 1.8em;\r\n    margin-right: .8em;\r\n    border-radius: 4px 0 0 4px;\r\n  }"; });
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,35 @@
-import {Hero} from './hero';
+import { autoinject } from 'aurelia-framework';
 
+import {Hero} from './hero';
+import { HeroService } from './hero-service';
+
+@autoinject()
 export class App {
+
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero = null;
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  activate() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+
+    /* original version, pre appendix: take it slow in https://angular.io/docs/ts/latest/tutorial/toh-pt4.html
+      this.heroService.getHeroes()
+                    .then(heroes => this.heroes = heroes);    
+    */                    
+    this.heroService.getHeroesSlowly()
+                    .then(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
